04936b2f7212b1a26a75abb5d3c87ae36ed7ce14
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2023-09-06 19:58:10 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 304 ms, Memory: 118024 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.py
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.py
@@ -1,10 +1,11 @@
+### 한번 틀린 이유: di,dj 쓸 때 좌표 실수
 import sys
 from collections import deque
 input = sys.stdin.readline
 ### 기존 입력값 받기
 N, M, D = map(int, input().rstrip().split())
 arr = []
-enemylst = []
+enemylst = [] ### 적이 있는 리스트 생성
 for i in range(N):
     line = input().rstrip().split()
     for j in range(M):
@@ -15,15 +16,13 @@ for i in range(N):
 ### 궁수 조합에서 제거 가능한 적 수 계산하기
 answer = 0
 def killenemy(lst):
-    killcnt = 0
-    enemies = deque(enemylst)
-    while enemies:
+    killcnt, enemies = 0, enemylst ### 초기화
+    while enemies: ### 적이 없어질 때까지
         removelst = set()
-        for archer_j in lst:
+        for archer_j in lst: ### 생성된 조합에서 궁수 한명을 뽑아서 한턴 진행
             visited = [[0]*M for _ in range(N)]
             findenemy = deque()
-            dist = 1
-            go = True
+            dist, go = 1, True
             while dist<=D:
                 if dist == 1:
                     if (N-1, archer_j) in enemies:
@@ -45,27 +44,22 @@ def killenemy(lst):
                             elif 0<=ni<N and 0<=nj<M and visited[ni][nj] == 0:
                                 visited[ni][nj] = 1
                                 findenemy.append((ni,nj))
-                        if not go:
-                            break
-                    if not go:
-                        break
+                        if not go: break
+                    if not go: break
                     dist += 1
         newenemy = []
         for enem_i, enem_j in enemies:
             if (enem_i, enem_j) in removelst: killcnt += 1
-            elif enem_i+1 >= N: continue
-            else: newenemy.append((enem_i+1, enem_j))
+            elif enem_i+1 < N: newenemy.append((enem_i+1, enem_j))
         enemies = newenemy
-        # print('한턴당', enemies, killcnt)
     return killcnt
 ### 궁수 조합 만들기
 answer = 0
 def archercomb(n, idx, lst):
     global answer
     if n == 3:
-        killcount = killenemy(lst)
-        answer = max(answer, killcount)
-        # print('궁수조합', lst, killcount)
+        answer = max(answer, killenemy(lst))
+        return
     for i in range(idx+1, M):
         archercomb(n+1, i, lst+[i])
 archercomb(0,-1,[])
