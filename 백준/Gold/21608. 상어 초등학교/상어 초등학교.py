f8ab130ce7ed050d994e64df47594b08448b809f
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2023-09-23 11:24:30 +0900

    [Gold V] Title: 상어 초등학교, Time: 440 ms, Memory: 119868 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -1,38 +1,42 @@
-import sys
+import sys,heapq
 input = sys.stdin.readline
 N = int(input().rstrip())
-favor = {}  ## 좋아하는 학생 리스트를 딕셔너리로 받기 위함
+favor = {}
 for _ in range(N*N):
     lst = list(map(int, input().rstrip().split()))
     favor[lst[0]] = lst[1:]
-visited = [[0]*N for _ in range(N)]
-for student, favorites in favor.items(): ### 좋아하는 학생 리스트 탐방
+visited = [[[0,0] for _ in range(N)] for _ in range(N)]
+# print(favor)
+for student, favorites in favor.items():
     seatlst = []
     for i in range(N):
         for j in range(N):
-            if visited[i][j] == 0: ### 아직 자리 지정이 되지 않았으면
+            if visited[i][j][0] == 0:
                 lovers = 0
                 empty = 0
-                for di, dj in [(0,1),(0,-1), (1,0),(-1,0)]: ### 인접 자리 탐방
+                favorlst = []
+                for di, dj in [(0,1),(0,-1), (1,0),(-1,0)]:
                     ni, nj = i+di, j+dj
                     if 0<=ni<N and 0<=nj<N:
-                        ### 비어있으면 empty -1 이유는 heapq에서는 작을수록 우선순위를 가짐
-                        if visited[ni][nj] == 0:
+                        if visited[ni][nj][0] == 0:
                             empty -= 1
-                        elif visited[ni][nj] in favorites: ### 같은 원리로 좋아하는 학생수 빼기
+                        elif visited[ni][nj][0] in favorites:
                             lovers -= 1
-                seatlst.append((lovers,empty,i,j))### 우선순위 자리 뽑기
-    l, e, a, b = sorted(seatlst)[0]
-    visited[a][b] = student
+                        if visited[ni][nj][0] != 0 and student in favor[visited[ni][nj][0]]:
+                            favorlst.append((ni,nj))
+                heapq.heappush(seatlst,(lovers,empty,i,j,favorlst))
+    l, e, a, b, f_lst = heapq.heappop(seatlst)
+    visited[a][b][0] = student
+    visited[a][b][1] = l*(-1)
+    for ki, kj in f_lst:
+        visited[ki][kj][1] += 1
 
+    # # print(student, lovers)
+    # for l in visited:
+    #     print(l)
 answer = 0
 satisfylst = [0,1,10,100,1000]
 for i in range(N):
     for j in range(N):
-        cnt = 0
-        for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:  ### 인접 자리 탐방
-            ni, nj = i + di, j + dj
-            if 0 <= ni < N and 0 <= nj < N and visited[ni][nj] in favor[visited[i][j]]:
-                cnt += 1
-        answer += satisfylst[cnt]
+        answer += satisfylst[visited[i][j][1]]
 print(answer)
