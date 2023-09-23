0a58c860ed927a944522751c9db7fdb3d73e619f
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2023-09-23 12:17:23 +0900

    [Gold III] Title: 연구소 3, Time: 424 ms, Memory: 118196 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -14,47 +14,48 @@ for i in range(N):
         elif lst[j] == 0:
             emptycnt += 1
     arr.append(lst)
-### M개의 바이러스가 퍼지는 속도 구하기
-def bfs(active):
+### 조합 수 만들기
+comblst = []
+def comb(n, lst, idx):
+    if n==M:
+        comblst.append(lst)
+        return
+    for i in range(idx+1, len(viruslst)):
+        comb(n+1, lst+[viruslst[i]], i)
+comb(0,[],-1)
+def bfs():
+    unactive = []
     delcnt = emptycnt
     q = deque(active)
     visited = [['-']*N for _ in range(N)]
-    maxnum = 0
     for i, j in q:
         visited[i][j] = 0
     while q:
         i, j = q.popleft()
-        time = visited[i][j]
         for di, dj in [(0,1),(0,-1),(1,0),(-1,0)]:
             ni, nj = i+di, j+dj
             if 0<=ni<N and 0<=nj<N and arr[ni][nj] != 1 and (visited[ni][nj] == '-'):
                 if arr[ni][nj] == 0:
-                    maxnum = time+1
-                    visited[ni][nj] = time + 1
+                    visited[ni][nj] = visited[i][j] + 1
                     delcnt -= 1
                     q.append((ni,nj))
                 elif arr[ni][nj] == 2:
-                    visited[ni][nj] = time + 1
+                    visited[ni][nj] = visited[i][j] + 1
+                    unactive.append((ni,nj))
                     q.append((ni,nj))
+    for i, j in unactive:
+        visited[i][j] = '-'
+    # print(active, delcnt, max(x if x!='-' else 0 for y in visited for x in y))
     if delcnt == 0:
-        return maxnum
+        return max(x if x!='-' else 0 for y in visited for x in y)
     return -1
-### 조합 수 만들어서 진행
-def comb(n, lst, idx):
-    global zero, answer
-    if n==M:
-        ans = bfs(lst)
-        if ans != -1:
-            zero = True
-            answer = min(answer, ans)
-        return
-    for i in range(idx+1, len(viruslst)):
-        comb(n+1, lst+[viruslst[i]], i)
+
 answerlst = []
-zero = False
-answer = N**2
-comb(0,[],-1)
-if not zero:
-    print(-1)
+for active in comblst:
+    ans = bfs()
+    if ans != -1:
+        answerlst.append(ans)
+if answerlst:
+    print(min(answerlst))
 else:
-    print(answer)
+    print(-1)
