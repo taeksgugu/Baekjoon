7ff5772c3752d885d3c1c412574cebba10e69993
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2023-09-24 16:06:05 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 536 ms, Memory: 118604 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -2,6 +2,17 @@ import sys
 input = sys.stdin.readline
 N, M = map(int, input().rstrip().split())
 arr = [list(map(int, input().rstrip().split())) for _ in range(N)]
+### 좌표 조정 함수 만들기 (다 연결되기 때문)
+def control(i):
+    if i < 0:
+        if (-1)*i % N == 0:
+            return 0
+        else:
+            return i + ((-1) * i // N + 1) * N
+    elif 0<=i<N:
+        return i
+    else:
+        return i % N
 ### 방향 리스트 만들기 ←, ↖, ↑, ↗, →, ↘, ↓, ↙
 dirlst = [(0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1)]
 
@@ -12,17 +23,15 @@ cloudlst = [(N-1,0), (N-1,1), (N-2,0), (N-2,1)]
 for _ in range(M):
     di, si = map(int, input().rstrip().split())
     di, dj = map(lambda x: x*si, dirlst[di-1])
-    ### di 방향으로 si칸 이동 & 구름 있는 칸 바구니의 물 양 증가
-    check = set()
-    while cloudlst:
-        i, j = cloudlst.pop()
-        ci, cj = (i+di)%N, (j+dj)%N ### 좌표 조정-> 이어지기 때문
-        check.add((ci,cj))
-        arr[ci][cj] += 1
+    ### di 방향으로 si칸 이동
+    cloudlst = [(control(i+di), control(j+dj)) for i,j in cloudlst]
+    ### 구름 있는 칸 바구니의 물 양 증가
+    for cloud_i, cloud_j in cloudlst:
+        arr[cloud_i][cloud_j] += 1
 
     ### 구름 사라짐 -> 나중에 사라진 칸 체크를 위해 남겨놓음
     ### 2에서 물이 증가한 칸 물복사 시전
-    for cloud_i, cloud_j in check:
+    for cloud_i, cloud_j in cloudlst:
         cnt = 0
         for ki, kj in [(-1,-1), (-1,1), (1,-1), (1,1)]:
             ni, nj = cloud_i+ki, cloud_j+kj
@@ -31,10 +40,12 @@ for _ in range(M):
         arr[cloud_i][cloud_j] += cnt
 
     ### 2이상인 칸 구름 생성
+    newcloud = []
     for i in range(N):
         for j in range(N):
-            if (i,j) not in check and arr[i][j] >=2:
-                cloudlst.append((i,j))
+            if (i,j) not in cloudlst and arr[i][j] >=2:
+                newcloud.append((i,j))
                 arr[i][j] -= 2
+    cloudlst = newcloud
 
 print(sum(x for y in arr for x in y))
