144fafd1a2ef8a669ee6e04d512c6949b6f6098e
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2023-09-28 16:58:09 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 312 ms, Memory: 132208 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
@@ -20,12 +20,13 @@ def solve():
         for i, j in arrdic.keys():
             lst = arrdic[(i,j)]
             for m, s, d in lst:
-                di, dj = dirlst[d] ### 방향 좌표 가져오기
-                ni, nj = (i+di*s)%N, (j+dj*s)%N ### 1행과 N행이 연결, 1열과 N열이 연결되어있기 때문에 해당 방식으로 좌표 처리
+                di, dj = dirlst[d]
+                ni, nj = (i+di*s)%N, (j+dj*s)%N
                 if (ni,nj) in newdic:
                     newdic[(ni,nj)].append((m,s,d))
                 else:
                     newdic[(ni,nj)] = [(m,s,d)]
+        # print('이동 후', newdic)
         ### 같은 칸 2개 이상일 때
         ballcnt = 0 ### 남은 파이어볼 갯수 파악 -> 만약 0개가 된다면 while문을 break해도 됨
         for i, j in newdic.keys():
@@ -49,15 +50,13 @@ def solve():
                     ballcnt += 4
             else:
                 ballcnt += cnt
-        if ballcnt == 0: ### 이미 0개일 경우 더 이상 해도 의미가 없기 때문에 바로 return
+        if ballcnt == 0:
             return 0
         arrdic = newdic
+        # print(time, arrdic)
     answer = 0
     for balls in arrdic.values():
         for m,s,d in balls:
             answer += m
     return answer
 print(solve())
-### 해당 코드 포인트 : 2차원 리스트를 생성해서 파이어볼의 위치를 기록하지 않고 딕셔너리로 좌표 추적
-###                 입력 받을 때 바로 계산하기 편하게 -1 해서 좌표 입력받음
-###                 합을 구하는 경우, map(sum, lst)를 고려했으나, 홀수 짝수 처리를 따로 해야해서 그냥 for문 사용
