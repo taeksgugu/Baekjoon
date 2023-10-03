44ffd0fc98101ea536deade687e867342ea50a33
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2023-10-03 14:10:54 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 196 ms, Memory: 124496 KB -BaekjoonHub

--- 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.py
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.py
@@ -1,42 +1,92 @@
-### 1차 시도 : 124496kb 196ms
-### 진짜 단순 무식하게 직접 구현했다.
-### 아이디어 방식은 외곽 줄의 값들을 리스트로 만들어서 R번 회전시킨 다음에 다시 넣는 방식 사용
-### R의 크기가 너무 컸지만 리스트 길이의 나머지 연산을 통해 단축
-### 2차 시도 :
-### 겹치는 부분을 합칠 수 있다고 판단 즉, 함수 2개를 합침
-### 합치는 과정에서 범위 및 인덱싱하는거 계산이 헷갈림
 import sys
 input = sys.stdin.readline
-
-### 입력 받기
-N, M, R = map(int,input().rstrip().split())
+N, M, T = map(int, input().rstrip().split())
 arr = [input().rstrip().split() for _ in range(N)]
 
-### R번 회전시키는 함수
-def turns():
-    for x in range(min(N//2, M//2)):
-        line = [arr[x][i] for i in range(x, M-x)]
-        line += [arr[i][M-1-x] for i in range(x+1, N-1-x)]
-        line += [arr[N-1-x][i] for i in range(x, M-x)][::-1]
-        line += [arr[i][x] for i in range(x+1, N-1-x)][::-1]
+### 우:0 하:1 좌:2 상:3
+def chk():
+    move = 0
+    R, C = N, M
+    i, j = 0, -1
+    dir = 0
+    linelst = []
+    while R>0 or C>0:
+        if dir == 0:
+            lst = []
+        if dir == 0 or dir == 2:
+            for _ in range(C,0,-1):
+                if dir == 0:
+                    j += 1
+                else:
+                    j -= 1
+                lst.append(arr[i][j])
+                move += 1
+                if move == N*M:
+                    linelst.append(lst)
+                    return linelst
+        R -= 1
+        dir += 1
+        if dir == 1 or dir == 3:
+            for _ in range(R,0,-1):
+                if dir == 1:
+                    i += 1
+                else:
+                    i -= 1
+                lst.append(arr[i][j])
+                move += 1
+                if move == N*M:
+                    linelst.append(lst)
+                    return linelst
+        C -= 1
+        dir = (dir+1)%4
+        if dir == 0:
+            linelst.append(lst)
 
-        length = len(line)
-        newline = line[R%length:] + line[:R%length]
+### 회전 시키는 함수
+linelst = chk()
+for i in range(len(linelst)):
+    lines = linelst[i]
+    length = len(lines)
+    linelst[i] = lines[T%length:] + lines[:T%length]
 
-        idx = 0
-        for i in range(x, M-x):
-            arr[x][i] = newline[idx]
-            idx += 1
-        for i in range(x+1, N-1-x):
-            arr[i][M-1-x] = newline[idx]
-            idx += 1
-        for i in range(M-x-1, x-1, -1):
-            arr[N-1-x][i] = newline[idx]
-            idx += 1
-        for i in range(N-1-1-x, x, -1):
-            arr[i][x] = newline[idx]
-            idx += 1
-    return arr
 
-for i in turns():
-    print(*i)
+def make():
+    newarr = [[0]*M for _ in range(N)]
+    move = 0
+    R, C = N, M
+    i, j = 0, -1
+    dir = 0
+    idx, jdx = 0, 0
+    while R>0 or C>0:
+        if dir == 0 or dir == 2:
+            for _ in range(C,0,-1):
+                if dir == 0:
+                    j += 1
+                else:
+                    j -= 1
+                newarr[i][j] = linelst[idx][jdx]
+                move += 1
+                jdx += 1
+                if move == N*M:
+                    return newarr
+        R -= 1
+        dir += 1
+        if dir == 1 or dir == 3:
+            for _ in range(R,0,-1):
+                if dir == 1:
+                    i += 1
+                else:
+                    i -= 1
+                newarr[i][j] = linelst[idx][jdx]
+                move += 1
+                jdx += 1
+                if move == N*M:
+                    return newarr
+        C -= 1
+        dir = (dir+1)%4
+        if dir == 0:
+            idx += 1
+            jdx = 0
+
+for k in make():
+    print(*k)
