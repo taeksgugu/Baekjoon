a145bdfc8d6e40010b048d82ec15b4d2a0c69a5a
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2023-10-04 14:56:15 +0900

    [Gold II] Title: 주사위 윷놀이, Time: 1676 ms, Memory: 148148 KB -BaekjoonHub

--- 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.py
+++ 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.py
@@ -1,51 +1,54 @@
-board = [[0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38],
-         [10, 13, 16, 19],
-         [20, 22, 24],
-         [30, 28, 27, 26],
-         [25, 30, 35],
-         [40, 0]]
-result = 0
+import sys
+input = sys.stdin.readline
+### 입력
+dice = list(map(int, input().rstrip().split()))
 
-dice_list = list(map(int, input().split()))
+###윳놀이 바깥줄 리스트 만들기
+line = [i for i in range(0,42)] ### 0은 시작점, 41은 도착점
+line10 = [10, '13', '16', '19', '25', '30', '35', 40, '41']
+line20 = [20, '22', '24', '25', '30', '35', 40, '41']
+line30 = [30, '28', '27', '26', '25', '30', '35', 40, '41']
+line25 = ['25', '30', '35', 40, '41', '41', '41', '41', '41']
 
-
-def game_start(horses, dice_idx, acc):
-    global result
-
-    if dice_idx == 10:
-        result = max(result, acc)
+### 주사위 놀이 시작
+answer = 0
+def yoot(n, lst, score, belst):
+    global answer
+    # print(n, lst, score, edgecase)
+    if n == 10: ### 종료조건
+        # if answer < score:
+        #     print(n, lst, score, belst)
+        answer = max(answer, score)
         return
-    
-    loop = horses.index([0, 0]) + 1 if [0, 0] in horses else 4
-    
-    for idx, loc in enumerate(horses):
-        if idx >= loop:
-            break
-        
-        new_loc = [loc[0], loc[1] + dice_list[dice_idx]]
 
-        if new_loc == [0, 5]: new_loc = [1, 0]
-        elif new_loc == [0, 10]: new_loc = [2,0]
-        elif new_loc == [0, 15]: new_loc = [3, 0]
+    ### 돌 선택해서 움직이기
+    num = dice[n]
+    for i in range(4):
+        horse = lst[i]
+        if int(horse) >= 41: continue ### 이미 도착한 말
+        ### 해당 말이 파란색화살표를 사용했을 경우
+        if horse in line25:
+            idx = line25.index(horse)
+            next = line25[idx + num]
+        elif horse == 10 or horse in line10:
+            idx = line10.index(horse)
+            next = line10[idx + num]
+        elif horse == 30 or horse in line30:
+            idx = line30.index(horse)
+            next = line30[idx + num]
+        elif horse == 20 or horse in line20:
+            idx = line20.index(horse)
+            next = line20[idx + num]
         else:
-            while new_loc[1] >= len(board[new_loc[0]]):
-                if new_loc[0] in [0, 4]:
-                    new_loc[1] -= len(board[new_loc[0]])
-                    new_loc[0] = 5
+            next = horse + num*2
+        # print('다음', next, i)
+        ### 이동할 칸에 다른 말 있는 경우 확인
+        if int(next) < 41 and next not in lst: ### 다른 말이 해당 칸에 없으면 이동
+            yoot(n+1, lst[:i]+[next]+lst[i+1:], score+int(next), lst)
+        elif int(next) >= 41: ### 도착했을 경우
+            # print('도착')
+            yoot(n+1, lst[:i] + [41] + lst[i + 1:], score, lst)
 
-                elif 1 <= new_loc[0] <= 3:
-                    new_loc[1] -= len(board[new_loc[0]])
-                    new_loc[0] = 4
-
-                else:
-                    new_loc[1] = 1
-
-        if new_loc != [5, 1] and new_loc in horses:
-            continue
-
-        game_start(horses[:idx] + [new_loc] + horses[idx + 1:], dice_idx + 1, acc + board[new_loc[0]][new_loc[1]])
-
-
-game_start([[0, 0] for _ in range(4)], 0, 0)
-
-print(result)
+yoot(0, [0,0,0,0],0,[])
+# yoot(4, [28,28,28,28],0,[0,0,0,0])
+print(answer)
