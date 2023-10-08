c82e13dc86155d60319718a031bdb392003995e7
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2023-10-08 15:44:17 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 546 ms, Memory: 62,696 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
@@ -1,28 +1,28 @@
+### 입력 받기
+def cal(lst):
+    score = 0
+    for i in range(N//2):
+        for j in range(i+1, N//2):
+            a, b = lst[i], lst[j]
+            score += arr[a][b]
+            score += arr[b][a]
+    return score
+
+def cook(n, a, b):
+    global answer
+    if len(a)==len(b)==N//2:
+        scenergy_a = cal(a)
+        scenergy_b = cal(b)
+        differ = abs(scenergy_a - scenergy_b)
+        answer = min(differ, answer)
+        return
+    if n==N: return
+    cook(n+1, a+[n], b)
+    cook(n+1, a, b+[n])
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
-
-    def check(lst):
-        result = 0
-        for i in range(N//2):
-            for j in range(i+1, N//2):
-                a, b = lst[i], lst[j]
-                result += arr[a][b]
-                result += arr[b][a]
-        return result
-
     answer = 1e9
-    def cook(n, alst, blst):
-        global answer
-        if len(alst) == N//2 and len(blst) ==N//2:
-            acnt = check(alst)
-            bcnt = check(blst)
-            answer = min(abs(acnt-bcnt), answer)
-            return
-        if n == N: return
-        cook(n+1, alst+[n], blst)
-        cook(n+1, alst, blst+[n])
-
     cook(0,[],[])
     print(f"#{tc} {answer}")
