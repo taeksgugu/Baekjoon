42154f05aefc2747961cd6bd32302528536f6b2d
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2023-10-08 14:11:38 +0900

    [Unrated] Title: [모의 SW 역량테스트] 원자 소멸 시뮬레이션, Time: 5,412 ms, Memory: 118,924 KB -BaekjoonHub

--- SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.py
+++ SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.py
@@ -1,37 +1,38 @@
-dirlst = [(0, 1), (0, -1), (-1, 0), (1, 0)]
 T = int(input())
 for tc in range(1, T+1):
-
-    answer = 0
     N = int(input())
-    arr = []
+    dirlst = [(0,1), (0,-1), (-1,0), (1,0)]
+    atomlst = []
     for _ in range(N):
-        i, j, dir, energy = map(int, input().split())
-        i, j = (i+1000)*2, (j+1000)*2
-        arr.append([i,j, dir, energy])
-    while arr:
-        ### 0.5 씩 이동 후 해당 좌표 확인
-        deletelst = [] ### 정해진 지도 밖으로 나가는 리스트
-        visited = {}
-        for idx in range(len(arr)):
-            i, j, dir, energy = arr[idx]
-            di, dj = dirlst[dir]
-            ni, nj = i+di, j+dj
-            if ni<0 or ni>=4001 or nj<0 or nj>=4001:
-                deletelst.append(idx)
+        x, y, d, K = map(int, input().split())
+        atomlst.append(((x+1000)*2, (y+1000)*2, d, K))
+    # print(atomlst)
+    totalscore = 0
+    while True: ### 끝까지 진행 (0.5초 단위를 1초로 봄, 그대신 위에서 좌표 *2 진행)
+    # for _ in range(3):
+        ### 원자 이동
+        resultlst = {}
+        for ai, aj, ad, ak in atomlst:
+            di, dj = dirlst[ad]
+            ni, nj = ai+di, aj+dj
+            if 0<=ni<4001 and 0<=nj<4001:
+                if resultlst.get((ni,nj)): resultlst[(ni,nj)].append((ad, ak))
+                else: resultlst[(ni,nj)] = [(ad,ak)]
+
+        ### 충돌 여부 확인 및 다음 턴 넘길
+        newatomlst = []
+        for ci, cj in resultlst:
+            lst = resultlst[(ci,cj)]
+            if len(lst) == 1: ### 하나라면
+                cd, ck = lst[0]
+                newatomlst.append((ci, cj, cd, ck))
             else:
-                if (ni,nj) in visited:
-                    visited[(ni,nj)] += 1
-                else:
-                    visited[(ni,nj)] = 1
-            arr[idx][0], arr[idx][1] = ni, nj
-        finalarr = []
-        for meet_idx in range(len(arr)):
-            if meet_idx not in deletelst:
-                ci, cj, cdir, cenergy = arr[meet_idx]
-                if visited[(ci,cj)] >= 2:
-                    answer += cenergy
-                else:
-                    finalarr.append(arr[meet_idx])
-        arr = finalarr
-    print(f"#{tc} {answer}")
+                deletepower = 0
+                for _, ck in lst:
+                    deletepower += ck
+                totalscore += deletepower
+        # print(resultlst)
+        if len(newatomlst) == 0: break
+        atomlst = newatomlst
+
+    print(f"#{tc} {totalscore}")
