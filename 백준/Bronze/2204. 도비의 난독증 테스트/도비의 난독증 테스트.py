9bfa3189d2ade365f851b35e35d8b113b082824b
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2023-10-12 20:38:05 +0900

    [Bronze I] Title: 도비의 난독증 테스트, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/2204. 도비의 난독증 테스트/도비의 난독증 테스트.py
+++ 백준/Bronze/2204. 도비의 난독증 테스트/도비의 난독증 테스트.py
@@ -1,6 +1,8 @@
+import sys
+input = sys.stdin.readline
 while True:
     N = int(input())
     if N == 0: break
-    lst = [input() for _ in range(N)]
+    lst = [input().rstrip() for _ in range(N)]
     lst.sort(key=lambda x: x.lower())
     print(lst[0])
