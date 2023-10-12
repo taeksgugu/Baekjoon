81bcaf27b9598e1a8a58029db56cd4ae94ec955a
taeksgugu <58875794+taeksgugu@users.noreply.github.com>
2023-10-12 14:17:07 +0900

    [Gold II] Title: 얼음 미로, Time: 512 ms, Memory: 127816 KB -BaekjoonHub

--- 백준/Gold/20926. 얼음 미로/얼음 미로.py
+++ 백준/Gold/20926. 얼음 미로/얼음 미로.py
@@ -1,3 +1,6 @@
+import heapq, sys
+input = sys.stdin.readline
+
 W, H = map(int, input().split())
 arr = [list(input()) for _ in range(H)]
 
@@ -11,42 +14,35 @@ for i in range(H):
             break
     if flag: break
 
-visited = [[[-1]*W for _ in range(H)] for _ in range(4)]
+visited = [[1e9]*W for _ in range(H)]
 dirlst = [(-1,0), (1,0), (0,-1), (0,1)]
-cango = [[2,3], [2,3], [0,1], [0,1]]
-import heapq
+
 def dijkstra():
-
-    q = [(0,0,ti,tj),(0,1,ti,tj),(0,2,ti,tj),(0,3,ti,tj)]
+    q = [(0,ti,tj)]
     heapq.heapify(q)
-    answer = 1e9
-    for i in range(4):
-        visited[i][ti][tj] = 0
+    visited[ti][tj] = 0
     while q:
-        cur_cnt,cur_d, ci,cj = heapq.heappop(q)
+        cur_dist, ci,cj = heapq.heappop(q)
         if arr[ci][cj] == 'E':
-            return answer
-        di, dj = dirlst[cur_d]
-        k = 1
-        dist = cur_cnt
-        while True:
-            ni, nj = ci+di*k, cj+dj*k
-            if ni<0 or ni>=H or nj<0 or nj>=W or arr[ni][nj] == 'H': break
-            if arr[ni][nj] == 'E':
-                # print('찾음')
-                answer = min(answer, dist)
-                heapq.heappush(q, (dist, cur_d, ni, nj))
-                break
-            if arr[ni][nj] == 'R':
-                for idx in cango[cur_d]:
-                    if visited[idx][ni-di][nj-dj]==-1 or visited[idx][ni-di][nj-dj]>dist:
-                        visited[idx][ni-di][nj-dj] = dist
-                        heapq.heappush(q, (dist, idx, ni-di, nj-dj))
-                break
-            else:
-                k += 1
-                dist += int(arr[ni][nj])
+            return cur_dist
 
-    if answer == 1e9: return -1
-    else: return answer
+        for di, dj in dirlst:
+            k = 1
+            dist = cur_dist
+            while True:
+                ni, nj = ci+di*k, cj+dj*k
+                if ni<0 or ni>=H or nj<0 or nj>=W or arr[ni][nj] == 'H': break
+                if arr[ni][nj] == 'E':
+                    heapq.heappush(q, (dist, ni, nj))
+                    break
+                if arr[ni][nj] == 'R':
+                    if visited[ni-di][nj-dj]>dist:
+                        visited[ni-di][nj-dj] = dist
+                        heapq.heappush(q, (dist, ni-di, nj-dj))
+                    break
+                else:
+                    k += 1
+                    dist += int(arr[ni][nj])
+
+    return -1
 print(dijkstra())
